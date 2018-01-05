a852582f58cdb1b32f00a7d23bd982bdd342d900
J. S. Oishi <jsoishi@gmail.com>
2018-01-05 16:37:42 -0500

    updated plot to show both users and dev lists

--- tools/users_messages_stats.py
+++ tools/users_messages_stats.py
@@ -3,23 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('modern')
 
-fname = "dedalus-users.mbox"
+fname_users = "dedalus-users.mbox"
+fname_dev = "dedalus-dev.mbox"
 
-dates = []
-email = []
+users = mailbox.mbox(fname_users)
+dev = mailbox.mbox(fname_dev)
+mailboxes = {'users':users, 'dev':dev}
+s = {}
+for k,mb in mailboxes.items():
+    dates = []
+    email = []
 
-mb = mailbox.mbox(fname)
-for m in mb:
-    dates.append(pd.to_datetime(m['date']))
-    email.append(m['from'])
+    for m in mb:
+        dates.append(pd.to_datetime(m['date']))
+        email.append(m['from'])
 
-s = pd.Series(email,index=dates)
+    s[k] = pd.Series(email,index=dates)
+
+print("users: {:d} unique email addresses.".format(len(s['users'].unique())))
+print("dev: {:d} unique email addresses.".format(len(s['dev'].unique())))
 
 # bin into months, get message counts
-message_counts = s.resample('M').count()
-print("{:d} unique email addresses.".format(len(s.unique())))
+message_counts = {}
+for k in s.keys():
+    message_counts[k] = s[k].resample('M').count()
 
 fig = plt.figure()
-p = message_counts[:-1].plot() # trim off Jan 2018
-p.set_ylabel('Messages to dedalus-users')
+p = message_counts['users'][:-1].plot(label='dedalus-users') # trim off Jan 2018
+p = message_counts['dev'][:-1].plot(label='dedalus-dev') # trim off Jan 2018
+p.set_ylim(0,100)
+p.set_ylabel('Messages to dedalus lists')
+p.legend(loc='upper left')
 fig.savefig('message_counts.png',dpi=300)
